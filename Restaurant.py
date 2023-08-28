9d6b150789828c41965e65a963be79a368e0ac93
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-08-28 07:28:56 +0300

    added restraunt class

--- Restaurant.py
+++ Restaurant.py
@@ -48,3 +48,31 @@ class Customer:
             if customer.given_name == name:
                 matching_customers.append(customer)
         return matching_customers
+
+
+class Restaurant:
+    all_restaurants = []
+
+    def __init__(self, name):
+        self.name = name
+        self.reviews = []
+        Restaurant.all_restaurants.append(self)
+
+    def name(self):
+        return self.name
+
+    def reviews(self):
+        return self.reviews
+
+    def customers(self):
+        reviewed_customers = []
+        for review in self.reviews:
+            reviewed_customers.append(review.customer)
+        return list(set(reviewed_customers))
+
+    def average_star_rating(self):
+        total_ratings = sum(review.rating for review in self.reviews)
+        num_reviews = len(self.reviews)
+        if num_reviews == 0:
+            return 0
+        return total_ratings / num_reviews
